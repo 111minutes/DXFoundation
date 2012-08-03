66df935686428cd205ffcb7a028bab781541c40a
Sergey Zenchenko <s.zenchenko@111min.com>
2012-08-03 15:01:57 +0300

    Initial Commit

---
+++ DXFoundation/DXFoundation.h
@@ -0,0 +1,13 @@
+//
+//  DXFoundation.h
+//  DXFoundation
+//
+//  Created by Sergey Zenchenko on 8/3/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DXFoundation : NSObject
+
+@end
