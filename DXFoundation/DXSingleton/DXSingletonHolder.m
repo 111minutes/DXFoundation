9dda9f7a5790cb7b5ae7bb3ff6fd3038df9e534a
Max Mashkov <mashkovmax@gmail.com>
2012-11-15 10:49:20 +0200

    creating new shared instance fix

--- DXFoundation/DXSingleton/DXSingletonHolder.m
+++ DXFoundation/DXSingleton/DXSingletonHolder.m
@@ -9,8 +9,8 @@
 
 @interface DXSingletonHolder ()
 
-@property (nonatomic, strong) NSDictionary *singltonesClassPairs;
-@property (nonatomic, assign) dispatch_queue_t singltoneQueue;
+@property (nonatomic, strong) NSDictionary *singletonsClassPairs;
+@property (nonatomic, assign) dispatch_queue_t singletonQueue;
 @end
 
 @implementation DXSingletonHolder
@@ -27,15 +27,15 @@
 
 - (void)dealloc
 {
-    dispatch_release(self.singltoneQueue);
+    dispatch_release(self.singletonQueue);
 }
 
 - (id)init
 {
     self = [super init];
     if (self) {
-        self.singltonesClassPairs = [NSMutableDictionary new];
-        self.singltoneQueue = dispatch_queue_create("com.111min.foundation.singltone_queue", DISPATCH_QUEUE_CONCURRENT);
+        self.singletonsClassPairs = [NSMutableDictionary new];
+        self.singletonQueue = dispatch_queue_create("com.111min.foundation.singleton_queue", DISPATCH_QUEUE_CONCURRENT);
     }
     return self;
 }
@@ -44,14 +44,14 @@
 {
     __block id instance;
     
-    dispatch_sync(self.singltoneQueue, ^{
-        instance = [self.singltonesClassPairs valueForKey:NSStringFromClass(requiredClass)];
+    dispatch_sync(self.singletonQueue, ^{
+        instance = [self.singletonsClassPairs valueForKey:NSStringFromClass(requiredClass)];
     });
     
     if (!instance) {
-        dispatch_barrier_sync(self.singltoneQueue, ^{
-            instance = [requiredClass new];
-            [self.singltonesClassPairs setValue:instance forKey:NSStringFromClass(requiredClass)];
+        instance = [requiredClass new];
+        dispatch_barrier_sync(self.singletonQueue, ^{
+            [self.singletonsClassPairs setValue:instance forKey:NSStringFromClass(requiredClass)];
         });
     }
     
@@ -60,15 +60,15 @@
 
 - (void)destroyInstanceOfClass:(Class)requiredClass
 {
-    dispatch_barrier_async(self.singltoneQueue, ^{
-        [self.singltonesClassPairs setValue:nil forKey:NSStringFromClass(requiredClass)];
+    dispatch_barrier_async(self.singletonQueue, ^{
+        [self.singletonsClassPairs setValue:nil forKey:NSStringFromClass(requiredClass)];
     });
 }
 
 - (void)destroyAll
 {
-    dispatch_barrier_async(self.singltoneQueue, ^{
-        self.singltonesClassPairs = [NSMutableDictionary new];
+    dispatch_barrier_async(self.singletonQueue, ^{
+        self.singletonsClassPairs = [NSMutableDictionary new];
     });
 }
 
