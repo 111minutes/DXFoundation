85c405d218ee6c69824fabadee0802c0381a8c30
Max Mashkov <mashkovmax@gmail.com>
2013-04-05 13:07:56 +0300

    added defines

---
+++ DXFoundation/DXFoundationBase/DXFoundationDefines.h
@@ -0,0 +1,10 @@
+//
+//  DXFoundationDefines.h
+//  DXFoundation
+//
+//  Created by Max Mashkov on 4/5/13.
+//  Copyright (c) 2013 111Minutes. All rights reserved.
+//
+
+#define CSWeakSelf __weak typeof(self)
+#define CSWeakRef(object) __weak typeof(object)
